2719c7f9ac31df479ffbafe37faecd8bead6b437
Rafael Goto <rafael916goto@hotmail.com>
2018-03-14 22:20:29 -0300

    Restrição negativa e Teste Hexa
    Condição para verificar se as entradas dos argumentos estão como esperados (numeros não negativos e bases entre 2 e 36)
    Restrição de ter como resposta um NULL no retorno da chamada verificaçaoBasica.
    Teste para verificação se for um hexadecimal.

--- lib.c
+++ lib.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "lib.h"
 
     void teste (int argc, char *argv[]) {
@@ -16,6 +17,13 @@
             t->baseAtual = atoi(argv[2]);
             t->baseFutura = atoi(argv[3]);
             t->numero = argv[1];
-        }
+        } if (t->baseAtual < 2 || t->baseFutura < 2 || t->baseAtual > 36 || t->baseFutura > 36) t = NULL;
         return t;
+    }
+
+    void isHexa(tConversion *t) {
+        if(isxdigit(*t->numero))
+            printf("hexadecimal: %s\n", t->numero);
+        else
+            printf("nao e hexa\n");
     }
