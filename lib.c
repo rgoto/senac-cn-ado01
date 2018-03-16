dc3de85b8818e26b2f79e2579055d0438ad8c3a4
Rafael Goto <rafael916goto@hotmail.com>
2018-03-16 15:52:20 -0300

    Update lib.c

--- lib.c
+++ lib.c
@@ -27,14 +27,45 @@
         return t;
     }
 
-    void removePunctuation (tConversion *t) {
+    void parametrization (tConversion *t) {
         for (int i = 0; i < t->lengthNumero; i++) {
             if ((t->numero[i] >= 32 && t->numero[i] <= 47) ||
                 (t->numero[i] >= 58 && t->numero[i] <= 64) ||
-                (t->numero[i] >= 92 && t->numero[i] <= 96) ||
-                (t->numero[i] >= 123 && t->numero[i] <= 126)) {
+                (t->numero[i] >= 91 && t->numero[i] <= 96) ||
+                (t->numero[i] >= 123 && t->numero[i] <= 126) ||
+                (t->numero[i] >= 127)) {
                     for (int j = i; j < t->lengthNumero; j++)
                         t->numero[j] = t->numero[j+1];
+
+                    t->lengthNumero = strlen(t->numero);
+                    i = -1;
                 }
         }
+
+        for (int i = 0; i < t->lengthNumero; i++)
+            t->numero[i] = toupper(t->numero[i]);
+    }
+
+    int verificacaoBase (tConversion *t) {
+        int num = 0;
+
+        if (t->baseAtual > 10) {
+            t->numBaseRef = A + (t->baseAtual - 11);
+                for (int i = 0; i < t->lengthNumero; i++)
+                    if ((int)t->numero[i] > t->numBaseRef)
+                        num = 1;
+        }
+        else {
+            t->numBaseRef = 58;
+            for (int i = 0; i < t->lengthNumero; i++)
+                if ((int) t->numero[i] > t->numBaseRef)
+                    num = 1;
+        }
+
+    return num;
+    }
+
+    int convesaoBase (tConversion *t) {
+
+        return t->numBaseRef;
     }
