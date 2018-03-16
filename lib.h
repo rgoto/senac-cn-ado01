e6a6b0df0d43e3c4ce1d0d0a488901eb4d8a3642
Rafael Goto <rafael916goto@hotmail.com>
2018-03-16 15:52:41 -0300

    Update lib.h

--- lib.h
+++ lib.h
@@ -2,12 +2,17 @@
 #define LIB_H
 
 typedef struct {
-    int baseAtual, baseFutura, lengthNumero;
+    int baseAtual, baseFutura, lengthNumero, numBaseRef;
     char *numero;
 } tConversion;
 
+enum numeros_alfabeto {
+    A = 65, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, X, W, Y, Z
+} numAlfabeto;
+
 void teste (tConversion *t);
 tConversion *verificacaoBasica (int argc, char *argv[]);
-void removePunctuation (tConversion *t);
+void parametrization (tConversion *t);
+int verificacaoBase (tConversion *t);
 
 #endif
